fa9702cd28b216f0c11b16cea1a5be14dbfa6365
Giovanna Tomé <giotome@uol.com.br>
2018-06-05 14:45:56 -0300

    Giovanna- atualização da Aba Team

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.Office.Interop;
 using Excel = Microsoft.Office.Interop.Excel;
 using DataTable = System.Data.DataTable;
+using System.Configuration;
+using Microsoft.Office.Core;
+using System.Runtime.InteropServices;
 
 namespace RefreshExcel1
 {
@@ -13,7 +16,7 @@ namespace RefreshExcel1
     {
         static void Openfile()
         {
-            string mySheet = @"C:\Users\g.pettenazzi.tome\OneDrive - Avanade\Giovanna\EINSTEIN\Power BI e TFS\DSS - Escopo do Projeto.xlsx";
+            string mySheet = ConfigurationSettings.AppSettings["EXCEL_PATH"];
             var excelApp = new Excel.Application();
             excelApp.Visible = true;
 
@@ -21,10 +24,25 @@ namespace RefreshExcel1
 
             Excel.Workbook sheet = books.Open(mySheet);
 
+            foreach (CommandBar bar in sheet.CommandBars)
+            {
+                if ("Team".Equals(bar.Name))
+                {
+                    foreach (CommandBarControl control in bar.Controls)
+                    {
+                        if ("IDC_REFRESH".Equals(control.Tag.ToUpper()))
+                            control.Execute();
+                    }
+                }
+            }
+
+
             sheet.RefreshAll();
             sheet.Save();
+            excelApp.Quit();
 
         }
+
         static void Main(string[] args)
         {
             Openfile();
